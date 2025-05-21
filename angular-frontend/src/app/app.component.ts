0fe5fa29964185c8063934bd11cd04e05cf46379
Xisco0809 <xisco0809@gmail.com>
2025-05-21 14:42:36 +0200

    Landing

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { LandingComponent } from "./module/landing/landing.component";
 import { CommonModule } from '@angular/common'; // Importa CommonModule
-//import { EntradasComponent } from './module/entradas/entradas.component';
+import { NavbarComponent } from './modules/shared/navbar/navbar.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule], // Añade LandingComponent y CommonModule
+  imports: [RouterOutlet, CommonModule, FooterComponent], // Añade LandingComponent y CommonModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-frontend';
